917b2e7949f80e5c457c2cb027e6637bce0b7c8f
Timon <tivi@ecco.com>
2026-09-25 11:46:22 +0200

    fix: update framework version with bugfix

--- build.sbt
+++ build.sbt
@@ -24,7 +24,7 @@ lazy val plugin = (project in file("."))
   .settings(
     name                                      := "arcane-stream-pull",
     idePackagePrefix                          := Some("com.sneaksanddata.arcane.stream_pull"),
-    libraryDependencies += "com.sneaksanddata" % "arcane-framework_3"              % "2.3.2-12-g0e7f6c7",
+    libraryDependencies += "com.sneaksanddata" % "arcane-framework_3"              % "2.3.2-13-g22b1144",
     libraryDependencies += "io.netty"          % "netty-tcnative-boringssl-static" % "2.0.74.Final",
 
     // bugfix for upgrade header
--- src/main/scala/models/app/PullStreamPluginContext.scala
+++ src/main/scala/models/app/PullStreamPluginContext.scala
@@ -1,11 +1,25 @@
 package com.sneaksanddata.arcane.pull_stream_plugin_context
 package models.app
 
-import com.sneaksanddata.arcane.framework.models.app.{DefaultPluginStreamContext, PluginStreamContext}
-import com.sneaksanddata.arcane.framework.models.settings.observability.DefaultObservabilitySettings
-import com.sneaksanddata.arcane.framework.models.settings.sink.DefaultSinkSettings
-import com.sneaksanddata.arcane.framework.models.settings.staging.DefaultStagingSettings
+import com.sneaksanddata.arcane.framework.models.app.{
+  DefaultOverrideStreamContext,
+  DefaultPluginStreamContext,
+  OverrideStreamContext,
+  PluginStreamContext
+}
+import com.sneaksanddata.arcane.framework.models.settings.observability.{
+  DefaultObservabilitySettings,
+  DefaultOverrideObservabilitySettings
+}
+import com.sneaksanddata.arcane.framework.models.settings.sink.{DefaultOverrideSinkSettings, DefaultSinkSettings}
+import com.sneaksanddata.arcane.framework.models.settings.sources.OverrideStreamSourceSettings
+import com.sneaksanddata.arcane.framework.models.settings.staging.{
+  DefaultOverrideStagingSettings,
+  DefaultStagingSettings
+}
 import com.sneaksanddata.arcane.framework.models.settings.streaming.{
+  DefaultOverrideStreamModeSettings,
+  DefaultOverrideThroughputSettings,
   DefaultStreamModeSettings,
   DefaultThroughputSettings
 }
@@ -27,11 +41,23 @@ case class PullStreamPluginContext(
     override val source: PullStreamSourceSettings
 ) extends DefaultPluginStreamContext(observabilityIn, stagingIn, streamModeIn, sinkIn, throughputIn) derives ReadWriter:
   // TODO: should be implemented when Operator supports overrides
-  override def merge(other: Option[PluginStreamContext]): PluginStreamContext = this
+  override def merge[OtherImpl <: OverrideStreamContext](other: Option[OtherImpl]): this.type = this
 
 object PullStreamPluginContext:
   def apply(value: String): PullStreamPluginContext = PluginStreamContext[PullStreamPluginContext](value)
 
   lazy val layer
       : ZLayer[Any, Throwable, PluginStreamContext & DatagramSocketConfig & MetricsConfig & DatadogPublisherConfig] =
-    PluginStreamContext.getLayer[PullStreamPluginContext]
+    PluginStreamContext.getLayer[PullStreamPluginContext, PullStreamOverrideStreamContext]
+
+/** Overrides for the stream specification, supplied by the operator via STREAMCONTEXT__SPEC_OVERRIDE.
+  */
+case class PullStreamOverrideStreamContext(
+    @key("observability") override val observability: Option[DefaultOverrideObservabilitySettings] = None,
+    @key("staging") override val staging: Option[DefaultOverrideStagingSettings] = None,
+    @key("streamMode") override val streamMode: Option[DefaultOverrideStreamModeSettings] = None,
+    @key("sink") override val sink: Option[DefaultOverrideSinkSettings] = None,
+    @key("throughput") override val throughput: Option[DefaultOverrideThroughputSettings] = None
+) extends DefaultOverrideStreamContext(streamMode, sink, staging, observability, throughput) derives ReadWriter:
+  // TODO: source overrides should be supported when Operator supports overrides
+  override val source: Option[OverrideStreamSourceSettings] = None
